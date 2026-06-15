23f832c3e7a6ccaa1fabbff5471246da218e03df
nengkya <nengkya@yahoo.com>
2026-06-15 22:40:41 +0700

    return flask.redirect(flask.url_for('login'))

--- flask_hello.py
+++ flask_hello.py
@@ -5,11 +5,24 @@ flask_application = flask.Flask(__name__)
 
 
 @flask_application.route('/')
+def index():
+    #http://example.com/admin/login
+    return flask.redirect('login')
+    return flask.redirect(flask.url_for('login'))
+
+
+@flask_application.route('/')
+def index2():
+    return 'index'
+
+
+@flask_application.route('/')
 def home():
     return '<h1>AMD Hackathlon deadline is Sunday 12 July 2026 </h1>'
 
 
 #must include passing both parameter of methods=['POST', 'GET']) or Method Not Allowed for the requested url
+#because the default is @flask_application.route('/login', methods=['GET'])
 @flask_application.route('/login', methods=['POST', 'GET'])
 def login():
     if flask.request.method == 'POST':
@@ -17,6 +30,11 @@ def login():
         username = flask.request.form['username']
         return f'{username}'
     return flask.render_template('form.html')
+
+
+@flask_application.route('/login')
+
+
 
 
 #handles rendering the form
@@ -34,7 +52,13 @@ def login_process():
 
 
 @flask_application.route('/hello')
-def hello():
+@flask_application.route('/hello/<name>')
+def hello(name=None):
+    return flask.render_template('hello.html', person=name)
+
+
+@flask_application.route('/hello')
+def hello2():
     name = flask.request.args.get('name', default = 'default string name = HaGa')
     name = flask.request.args.get('name', 'Flask')
     return f'Hello, {markupsafe.escape(name)} !'
@@ -53,9 +77,12 @@ def show_post(post_id):
     return f'post_id number {post_id}.'
 
 
+with flask_application.test_request_context():
+    print(flask.url_for('index'))
 
 
-
+with flask_application.test_request_context('/hello', method='POST'):
+    assert flask.request.path == '/hello'
 
 
 
