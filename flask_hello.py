cd55a806e51e41d7adc311d6e4b82c91fcb9eb33
nengkya <nengkya@yahoo.com>
2026-06-15 12:10:46 +0700

    fix <input name=ausername required> into <input name=username required>

--- flask_hello.py
+++ flask_hello.py
@@ -1,10 +1,13 @@
 import flask
 
+
 flask_application = flask.Flask(__name__)
+
 
 @flask_application.route('/')
 def hello():
     return '<h1>AMD Hackathlon deadline is Sunday 12 July 2026 </h1>'
+
 
 #must include passing both parameter of methods=['POST', 'GET']) or Method Not Allowed for the requested url
 @flask_application.route('/login', methods=['POST', 'GET'])
