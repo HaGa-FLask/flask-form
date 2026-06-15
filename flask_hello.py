27940b363ae59871eb54494457dfd5520d472d8c
nengkya <nengkya@yahoo.com>
2026-06-15 15:17:07 +0700

    return f'Hello, {markupsafe.escape(name)}'

--- flask_hello.py
+++ flask_hello.py
@@ -1,11 +1,11 @@
-import flask
+import flask, markupsafe
 
 
 flask_application = flask.Flask(__name__)
 
 
 @flask_application.route('/')
-def hello():
+def home():
     return '<h1>AMD Hackathlon deadline is Sunday 12 July 2026 </h1>'
 
 
@@ -17,3 +17,24 @@ def login():
         username = flask.request.form['username']
         return f'{username}'
     return flask.render_template('form.html')
+
+
+#handles rendering the form
+@flask_application.get('/login')
+def login_form():
+    return render_template('login.html')
+
+
+#handles processing the submitted credentials
+@flask_application.post('/login')
+def login_process():
+    username = request.form.get('username')
+    #process authentication...
+    return redirect(url_for('dashboard'))
+
+
+@flask_application.route('/hello')
+def hello():
+    name = flask.request.args.get('name', default = 'default string name = HaGa')
+    name = flask.request.args.get('name', 'Flask')
+    return f'Hello, {markupsafe.escape(name)}!'
